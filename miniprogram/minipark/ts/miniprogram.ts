8ce3750984d462d3d3b6764fab0e744ba9d42941
leaderman <miao.work2010@gmail.com>
2024-10-02 12:04:22 +0800

    添加登录和检查登录态方法

--- miniprogram/minipark/ts/miniprogram.ts
+++ miniprogram/minipark/ts/miniprogram.ts
@@ -2,6 +2,36 @@
  * 小程序 API。
  */
 const MiniProgram = {
+  /**
+   * 检查登录态 session_key 是否过期
+   * @returns {Promise<boolean>} 未过期返回 true，过期返回 false
+   */
+  checkSession(): Promise<boolean> {
+    return new Promise((resolve) => {
+      wx.checkSession({
+        success: () => resolve(true),
+        fail: () => resolve(false),
+      });
+    });
+  },
+
+  /**
+   * 调用接口获取登录凭证（code）
+   * @returns {Promise<string>} 登录凭证 code
+   */
+  login(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      wx.login({
+        success: (res) => resolve(res.code),
+        fail: (error) => reject(error),
+      });
+    });
+  },
+
+  /**
+   * 动态设置当前页面的标题
+   * @param {string} title 标题
+   */
   setNavigationBarTitle(title: string): Promise<void> {
     return new Promise((resolve, reject) => {
       wx.setNavigationBarTitle({
